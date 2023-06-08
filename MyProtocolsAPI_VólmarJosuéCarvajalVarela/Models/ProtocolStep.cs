addce382f51474e35fd3696e75f5c8eb5b4d66ac
Josue <josuev648@gmail.com>
2023-06-07 22:07:47 -0600

    Add project files.

---
+++ MyProtocolsAPI_VólmarJosuéCarvajalVarela/Models/ProtocolStep.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyProtocolsAPI_VólmarJosuéCarvajalVarela.Models
+{
+    public partial class ProtocolStep
+    {
+        public ProtocolStep()
+        {
+            ProtocolProtocols = new HashSet<Protocol>();
+        }
+
+        public int ProtocolStepsId { get; set; }
+        public string Step { get; set; } = null!;
+        public string? Description { get; set; }
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; } = null!;
+
+        public virtual ICollection<Protocol> ProtocolProtocols { get; set; }
+    }
+}
